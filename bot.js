613d8a33c4b5a9a681c7f5c56a6a6f9d967f30df
johnsjohns <o-anao@hotmail.com>
2020-11-06 17:27:41 -0300

    iniciando projeto

---
+++ bot.js
@@ -0,0 +1,22 @@
+const configure = require('dotenv').config();
+
+const tmi = require('tmi.js');
+const client = new tmi.Client({
+	options: { debug: true },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: configure.BOT_USERNAME,
+		password: configure.OAUTH_PASSWORD
+	},
+	channels: [ configure.CHANNELS ]
+});
+client.connect().catch(console.error);
+client.on('message', (channel, tags, message, self) => {
+	if(self) return;
+	if(message.toLowerCase() === '!hello') {
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+});
