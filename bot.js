a12b0e4207717d90f4fbd7b5032ee67a69968c4d
johnsjohns <o-anao@hotmail.com>
2020-11-06 18:32:00 -0300

    iniciando projeto

--- bot.js
+++ bot.js
@@ -13,11 +13,18 @@ const client = new tmi.Client({
 	},
 	channels: [ process.env.CHANNEL_NAME ]
 });
+client.on('connected', onConnectedHandler);
 client.connect().catch(console.error);
 client.on('message', (channel, tags, message, self) => {
 	if(self) return;
 	if(message.toLowerCase() === '!hello') {
 		client.say(channel, `@${tags.username}, heya!`);
 	}
+	
 });
 
+
+function onConnectedHandler (addr, port) {
+	console.log(`* Connected to ${addr}:${port}`);
+	client.say(process.env.CHANNEL_NAME, `Sejam bem vindos ao canal`);
+  }
