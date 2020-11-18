48a888143d1a79e55e5aa099f42271341883fc08
johnsjohns <o-anao@hotmail.com>
2020-11-17 23:13:46 -0300

    criado comando social

---
+++ commands/social.js
@@ -0,0 +1,7 @@
+module.exports = (client, channel) => {
+    client.say(channel, `Me sigam nas redes sociais:
+        https://twitter.com/jgfigueira
+        https://www.instagram.com/joaogilbertofigueira/
+        https://github.com/johnsjohns
+    `)
+}
