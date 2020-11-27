5171f6c74167d3378a5faa68132af670f3cdb12a
johnsjohns <o-ana@hotmail.com>
2020-11-26 23:11:58 -0300

    Comecando a logica de apagar mensagem safadinhas

---
+++ commands/comandos.js
@@ -0,0 +1,7 @@
+module.exports = (client, channel) => {
+    client.say(channel, `Comandos disponiveis: 
+    !roll roda dados /
+    !git mostra o github / 
+    !ola diz ola / 
+    !social mostra as redes sociais.`)
+}
